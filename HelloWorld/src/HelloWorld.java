a968adfd6ae8ff97778323ad1ca23e1eb0658cc3
Dharmatej Mikkilineni <jetamrahd@yahoo.com>
2017-02-21 21:42:18 -0800

    Test upload to github from eclipse.

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,34 @@
+/**
+ * Some easy hello world class.
+ */
+
+/**
+ * @author Dharmatej
+ */
+public class HelloWorld {
+
+	private int x;
+	private static final int OMEGA = 5;
+
+	/**
+	 * Hello world constructor.
+	 */
+	public HelloWorld() {
+		this.x = 5;
+		System.out.println(x);
+	}
+
+	/**
+	 * Main class to execute hello world.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+		System.out.println(OMEGA);
+	}
+}
+
+
+
+
+
